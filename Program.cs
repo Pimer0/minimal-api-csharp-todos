c2b02209464c40de6703f4015131f955ae9e5909
Pimer0 <paulmerandon@gmail.com>
2024-04-28 10:21:15 +0200

    init du projet, avec les verbes http

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+
+var builder = WebApplication.CreateBuilder(args);
+
+var app = builder.Build();
+
+app.MapGet("/getAll", () => "ici toutes les todo");
+app.MapGet("/getById", () => "ici une todo par son id");
+app.MapGet("/getActives", () => "ici toutes les todo actives");
+app.MapDelete("/delete", () => "ici supprimer une todo par son id");
+app.MapPost("/post", () => "créer une todo");
+app.MapPut("/put", () => "modifier une todo");
+
+
+app.Run();
