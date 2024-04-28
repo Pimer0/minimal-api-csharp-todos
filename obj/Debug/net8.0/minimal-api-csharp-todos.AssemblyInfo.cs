65d39247552230b18563b3bf0a02bafee82a910f
Pimer0 <paulmerandon@gmail.com>
2024-04-28 22:00:01 +0200

    Ceci est une branche qui finira delete

--- obj/Debug/net8.0/minimal-api-csharp-todos.AssemblyInfo.cs
+++ obj/Debug/net8.0/minimal-api-csharp-todos.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("minimal-api-csharp-todos")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12c62bec4eeef744cb5387813a544551e377f5d6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+65f90515f58017172cdd1c7132e89301e6cbee57")]
 [assembly: System.Reflection.AssemblyProductAttribute("minimal-api-csharp-todos")]
 [assembly: System.Reflection.AssemblyTitleAttribute("minimal-api-csharp-todos")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
